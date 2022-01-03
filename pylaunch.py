49e6706c046a36440293bbf8e25be5cb3b364689
MARKMENTAL <marky611@gmail.com>
2022-01-03 15:04:56 -0500

    Added pylaunch for launching the sim script: update 2

--- pylaunch.py
+++ pylaunch.py
@@ -1,19 +1,20 @@
 import os
 status = 1
 print("Launched by pylaunch")
-os.system("python3 pywrbs.py")
-choice = input("Would you like to go again?(Y/N)")
-while status == 1:
-	if os.name == "nt" and choice.lower() == "y" and status ==1:
+status = int(input("1.Python Version\n2.Python3 Version\n"))
+while status != 0:
+	if status == 1:
 		os.system("python pywrbs.py")
 		status = int(input("Press 0 to exit or 1 to keep playing:"))
 		if status != 1:
 			status = 0
-	elif choice.lower() == "y" and status == 1:
+	elif status == 2:
 		os.system("python3 pywrbs.py")
 		status = int(input("Press 0 to exit or 1 to keep playing:"))
 		if status != 1:
 			 status = 0
+		elif status == 1:
+			status = 2
 	else:
 		status = 0
 		print("Exiting...")
