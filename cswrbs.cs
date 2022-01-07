358a463a4cbd1ac0527c64360485796531421ad8
MARKMENTAL <marky611@gmail.com>
2022-01-07 18:52:16 -0500

    Added promo segments, some minor ui changes

--- cswrbs.cs
+++ cswrbs.cs
@@ -24,8 +24,8 @@ class CSWRBS {
         wrestler1 = "";
         wrestler2 = "";
         while (i <= matches) {
-            Console.WriteLine("Match " + i + " of " + fed + " " + eventna);
-            Console.WriteLine("Choose a match type\n1:1v1\n2:2v2");
+            Console.WriteLine("Segment " + i + " of " + fed + " " + eventna);
+            Console.WriteLine("Choose a match/segment type\n1:1v1 Match\n2:2v2 Match\n3:Promo Segment");
             int matchty = Convert.ToInt32(Console.ReadLine());
             if (matchty == 1) {
                 Console.WriteLine("1v1 Singles Match");
@@ -33,27 +33,27 @@ class CSWRBS {
                 wrestler1 = Console.ReadLine();
                 Console.WriteLine("Enter the name of the second wrestler:");
                 wrestler2 = Console.ReadLine();
-                Console.WriteLine("Enter a number representing wrestler 1's overness on a scale of 100:");
+                Console.WriteLine("Enter a number representing " +wrestler1 +"'s overness on a scale of 100: ");
                 w1ov = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter a number representing wrestler 2's overness on a scale of 100:");
+                Console.WriteLine("Enter a number representing " +wrestler2 +"'s overness on a scale of 100: ");
                 w2ov = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("If this is a title match enter the title's name, if not type a lowercase n:");
+                Console.WriteLine("If this is a title match enter the title's name, if not type a lowercase n: ");
                 titleop = Console.ReadLine();
                 Console.WriteLine("Choose a winner for the match:\n1:" + wrestler1 + "\n2:" + wrestler2);
                 winner = Convert.ToInt32(Console.ReadLine());
 
                 switch (winner) {
                 case 1:
-                    matchre = wrestler1 + " defeated " + wrestler2 + " by pinfall.";
+                    matchre = wrestler1 + " defeated " + wrestler2 + " by pinfall.\n";
                     if (titleop != "n") {
-                        matchre += "\nIn a match for the " + titleop + ".";
+                        matchre += "The match was for the " + titleop + ".\n";
                         titlebonus = 10;
                     }
                     break;
                 case 2:
-                    matchre = wrestler2 + " defeated " + wrestler1 + " by pinfall.";
+                    matchre = wrestler2 + " defeated " + wrestler1 + " by pinfall.\n";
                     if (titleop != "n") {
-                        matchre += "\nIn a match for the " + titleop + ".";
+                        matchre += "The match was for the " + titleop + ".\n";
                         titlebonus = 10;
                     }
                     break;
@@ -65,34 +65,34 @@ class CSWRBS {
                 matchre += "\nMatch Rating: " + matchra;
                 results[j] = matchre;
                 ratings[j] = matchra;
-                Console.WriteLine(matchre);
+                Console.WriteLine(matchre +"\n");
             } else if (matchty == 2) {
                 Console.WriteLine("2v2 Tag Team Match");
                 Console.WriteLine("Enter the name of the first team:");
                 team1 = Console.ReadLine();
                 Console.WriteLine("Enter the name of the second team:");
                 team2 = Console.ReadLine();
-                Console.WriteLine("Enter a number representing team 1's overness on a scale of 100:");
+                Console.WriteLine("Enter a number representing " +team1+"'s overness on a scale of 100: ");
                 t1ov = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter a number representing team 2's overness on a scale of 100:");
+                Console.WriteLine("Enter a number representing " +team2 +"'s overness on a scale of 100: ");
                 t2ov = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("If this is a title match enter the title's name, if not type a lowercase n:");
+                Console.WriteLine("If this is a title match enter the title's name, if not type a lowercase n: ");
                 titleop = Console.ReadLine();
                 Console.WriteLine("Choose a winner for the match:\n1:" + team1 + "\n2:" + team2);
                 winner = Convert.ToInt32(Console.ReadLine());
 
                 switch (winner) {
                 case 1:
-                    matchre = team1 + " defeated " + team2 + " by pinfall.";
+                    matchre = team1 + " defeated " + team2 + " by pinfall.\n";
                     if (titleop != "n") {
-                        matchre += "\nIn a match for the " + titleop + ".";
+                        matchre += "The match was for the " + titleop + ".\n";
                         titlebonus = 10;
                     }
                     break;
                 case 2:
-                    matchre = team2 + " defeated " + team1 + " by pinfall.";
+                    matchre = team2 + " defeated " + team1 + " by pinfall.\n";
                     if (titleop != "n") {
-                        matchre += "\nIn a match for the " + titleop + ".";
+                        matchre += "The match was for the " + titleop + ".\n";
                         titlebonus = 10;
                     }
                     break;
@@ -104,17 +104,46 @@ class CSWRBS {
                 matchre += "\nMatch Rating: " + matchra;
                 results[j] = matchre;
                 ratings[j] = matchra;
-                Console.WriteLine(matchre);
+                Console.WriteLine(matchre +"\n");
+            } else if (matchty == 3){
+                Console.WriteLine("Promo/Interview Segment");
+                Console.WriteLine("Enter the name of the first wrestler or team, they will be on the attacking side of this promo: ");
+                wrestler1 = Console.ReadLine();
+                Console.WriteLine("Enter the name of the second wrestler or team, they will be on the defending side of this promo: ");
+                wrestler2 = Console.ReadLine();
+                Console.WriteLine("Enter a number representing " +wrestler1 +"'s microphone skills on a scale of 100:");
+                w1ov = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a number representing " +wrestler2 +"'s microphone skills on a scale of 100:");
+                w2ov = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose a winner for the promo:\n1:" + wrestler1 + "\n2:" + wrestler2);
+                winner = Convert.ToInt32(Console.ReadLine());
+
+                switch (winner) {
+                case 1:
+                    matchre = wrestler1 + " insulted and intimidated " + wrestler2 + " to hype up their next match.\n";
+                    break;
+                case 2:
+                    matchre = wrestler2 + " came out to ambush and attack " + wrestler1 + ", ruining their promo.\n";
+                    break;
+                }
+                
+                matchra = w1ov + w2ov - rnd.Next(85, 110);
+                matchra += rnd.Next(8, 17);
+                matchre += "\nSegment Rating: " + matchra;
+                results[j] = matchre;
+                ratings[j] = matchra;
+                Console.WriteLine(matchre +"\n");
             }
             // incrementing counters after the match has been simmed
             i += 1;
             j += 1;
         }
 
+        // resetting match counter for printing event results
         i = 1;
-        Console.WriteLine("************************************\n" + fed + " " + eventna + " RESULTS\n************************************\n");
+        Console.WriteLine("\n************************************\n" + fed + " " + eventna + " RESULTS\n************************************\n");
         foreach(string result in results) {
-            Console.WriteLine("Match " + i + " Result:\n" + result + "\n");
+            Console.WriteLine("Segment " + i + " Result:\n" + result + "\n");
             i += 1;
         }
 
@@ -130,7 +159,7 @@ class CSWRBS {
         int matches;
         string fed, eventna;
         Console.WriteLine("Welcome to CSWRBS the free wrestling event booking simulator");
-        Console.WriteLine("Enter the amount of matches in the event:");
+        Console.WriteLine("Enter the amount of matches/segments in the event:");
         matches = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter the name or abbv. of the wrestling federation hosting the event:");
         fed = Console.ReadLine();
--- pywrbs.py
+++ pywrbs.py
@@ -1,9 +1,9 @@
 import random
 
 print("Welcome to PyWRBS the free wrestling event booking simulator")
-matches = int(input("Enter the amount of matches in the event:"))
-fed = input("Enter the name or abbv. of the wrestling federation hosting the event:")
-eventna = input("Now enter the event's name:")
+matches = int(input("Enter the amount of matches/segments in the event: "))
+fed = input("Enter the name or abbv. of the wrestling federation hosting the event: ")
+eventna = input("Now enter the event's name: ")
 
 i = 1
 results = []
@@ -12,71 +12,88 @@ finalrating = 0
 titlebonus = 0
 
 while i <= matches:
-	print ("Match " +str(i) +" of " +fed +" " +eventna);
-	print("Choose a match type\n1:1v1\n2:2v2")
-	matchty = int(input())
-	
-	if matchty == 1:
-		print("1v1 Singles Match")
-		wrestler1 = input("Enter the name of the first wrestler:")
-		wrestler2 = input("Enter the name of the second wrestler:")
-		w1ov = int(input("Enter a number representing wrestler 1's overness on a scale of 100:"))
-		w2ov = int(input("Enter a number representing wrestler 2's overness on a scale of 100:"))
-		titleop = input("If this is a title match enter the title's name, if not type a lowercase n: ")
-		winner = int(input("Choose a winner for the match:\n1:" +wrestler1+ "\n2:" +wrestler2+"\n"))
-		if winner == 1:
-			matchre = wrestler1 + " defeated " + wrestler2 + " by pinfall."
-			if titleop !="n":
-				matchre+= "For the " + titleop +"."
-				titlebonus = 10
-		elif winner == 2:
-			matchre = wrestler2 + " defeated " + wrestler1 + " by pinfall."
-			if titleop !="n":
-				matchre+= "For the " + titleop +"."
-				titlebonus = 10	
-		matchra = w1ov + w2ov - random.randint(85, 110)
-		matchra += random.randint(8,17) 
-		matchra += titlebonus
-		matchre += "\nMatch Rating: " + str(matchra)
-		results.append(matchre)
-		ratings.append(matchra)		
-		print(matchre)		
-	elif matchty == 2:
-		print("2v2 Tag Team Match")
-		team1 = input("Enter the name of the first team:")
-		team2 = input("Enter the name of the second team:")
-		t1ov = int(input("Enter a number representing team 1's overness on a scale of 100:"))
-		t2ov = int(input("Enter a number representing team 2's overness on a scale of 100:"))
-		titleop = input("If this is a title match enter the title's name, if not type a lowercase n: ")
-		winner = int(input("Choose a winner for the match:\n1:" +team1 + "\n2:" +team2+"\n"))
-		if winner == 1:
-			matchre = team1 + " defeated " + team2 + " by pinfall."
-			if titleop !="n":
-				matchre+= "For the " + titleop +"."
-				titlebonus = 10
-		elif winner == 2:
-			matchre = team2 + " defeated " + team1 + " by pinfall."
-			if titleop !="n":
-				matchre+= "For the " + titleop +"."
-				titlebonus = 10	
-		matchra = t1ov + t2ov - random.randint(85, 110)
-		matchra += random.randint(8 ,17) 
-		matchra += titlebonus
-		matchre += "\nMatch Rating: " + str(matchra)
-		results.append(matchre)
-		ratings.append(matchra)			
-		print(matchre)		
-		
-	i+=1
-	
+    print ("\nSegment " +str(i) +" of " +fed +" " +eventna);
+    print("Choose a match/segment type\n1:1v1 Match\n2:2v2 Match\n3:Promo Segment")
+    matchty = int(input())
+
+    if matchty == 1:
+        print("1v1 Singles Match")
+        wrestler1 = input("Enter the name of the first wrestler: ")
+        wrestler2 = input("Enter the name of the second wrestler: ")
+        w1ov = int(input("Enter a number representing " +wrestler1 +"'s overness on a scale of 100: "))
+        w2ov = int(input("Enter a number representing " +wrestler2 +"'s overness on a scale of 100: "))
+        titleop = input("If this is a title match enter the title's name, if not type a lowercase n: ")
+        winner = int(input("Choose a winner for the match:\n1:" +wrestler1+ "\n2:" +wrestler2+"\n"))
+        if winner == 1:
+            matchre = wrestler1 + " defeated " + wrestler2 + " in a match by pinfall.\n"
+            if titleop !="n":
+                matchre+= "The match was for the " + titleop +".\n"
+                titlebonus = 10
+        elif winner == 2:
+            matchre = wrestler2 + " defeated " + wrestler1 + " in a match by pinfall.\n"
+            if titleop !="n":
+                matchre+= "The match was for the " + titleop +".\n"
+                titlebonus = 10    
+        matchra = w1ov + w2ov - random.randint(85, 110)
+        matchra += random.randint(8,17) 
+        matchra += titlebonus
+        matchre += "\nMatch Rating: " + str(matchra)
+        results.append(matchre)
+        ratings.append(matchra)        
+        print(matchre)        
+    elif matchty == 2:
+        print("2v2 Tag Team Match")
+        team1 = input("Enter the name of the first team: ")
+        team2 = input("Enter the name of the second team: ")
+        t1ov = int(input("Enter a number representing " +team1 +"'s overness on a scale of 100: "))
+        t2ov = int(input("Enter a number representing " +team2 +"'s overness on a scale of 100: "))
+        titleop = input("If this is a title match enter the title's name, if not type a lowercase n: ")
+        winner = int(input("Choose a winner for the match:\n1:" +team1 + "\n2:" +team2+"\n"))
+        if winner == 1:
+            matchre = team1 + " defeated " + team2 + " in a match by pinfall.\n"
+            if titleop !="n":
+                matchre+= "The match was for the " + titleop +".\n"
+                titlebonus = 10
+        elif winner == 2:
+            matchre = team2 + " defeated " + team1 + " in a match by pinfall.\n"
+            if titleop !="n":
+                matchre+= "The match was for the " + titleop +".\n"
+                titlebonus = 10
+        matchra = t1ov + t2ov - random.randint(85, 110)
+        matchra += random.randint(8 ,17) 
+        matchra += titlebonus
+        matchre += "\nMatch Rating: " + str(matchra)
+        results.append(matchre)
+        ratings.append(matchra)
+        print(matchre)
+
+    elif matchty == 3:
+        print("Promo/Interview Segment")
+        wrestler1 = input("Enter the name of the first wrestler or team, they will be on the attacking side of this promo: ")
+        wrestler2 = input("Enter the name of the second wrestler or team, they will be on the defending side of this promo: ")
+        w1ov = int(input("Enter a number representing " +wrestler1 +"'s microphone skills on a scale of 100: "))
+        w2ov = int(input("Enter a number representing " +wrestler2 +"'s microphone skills on a scale of 100: "))
+        winner = int(input("Choose a winner for the promo:\n1:" +wrestler1+ "\n2:" +wrestler2+"\n"))
+        if winner == 1:
+            matchre = wrestler1 + " insulted and intimidated " + wrestler2 + " to hype up their next match.\n"
+        elif winner == 2:
+            matchre = wrestler2 + " came out to ambush and attack " + wrestler1 + ", ruining their promo.\n"
+        matchra = w1ov + w2ov - random.randint(85, 110)
+        matchra += random.randint(8,17) 
+        matchre += "\nSegment Rating: " + str(matchra)
+        results.append(matchre)
+        ratings.append(matchra)
+        print(matchre)
+    i+=1
+    
 i = 1
-print("************************************\n"+fed+" " +eventna+" RESULTS\n************************************\n")	
+print("\n************************************\n"+fed+" " +eventna+" RESULTS\n************************************\n")    
 for result in results:
-	print("Match " +str(i) + " Result:\n" +result +"\n")
-	i+=1
+    print("Segment " +str(i) + " Result:\n" +result +"\n")
+    i+=1
 
 for rating in ratings:
-	finalrating+=rating
+    finalrating+=rating
 
 finalrating = int(finalrating / matches)
-print("The final rating for " +fed +" " +eventna + " is: " +str(finalrating))	
+print("The final rating for " +fed +" " +eventna + " is: " +str(finalrating))    
